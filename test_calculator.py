61b3ffe03bc8ba48d50a088ef6f5fd9989d1d470
248shreyagupta-arch <248shreyagupta@gmail.com>
2026-09-21 10:27:51 +0530

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -1,10 +1,15 @@
-def calculate_total(marks):
-    return sum(marks)
-def calculate_average(marks):
-    return calculate_total(marks) / len(marks)
-def get_result(marks):
-    average = calculate_average(marks)
-    if average >= 40:
-        return "PASS"
-    else:
-        return "FAIL"
+from calculator import calculate_total
+from calculator import calculate_average
+from calculator import get_result
+def test_total():
+    marks = [80, 70, 90]
+    assert calculate_total(marks) == 240
+def test_average():
+    marks = [80, 70, 90]
+    assert calculate_average(marks) == 80
+def test_pass_result():
+    marks = [80, 70, 90]
+    assert get_result(marks) == "PASS"
+def test_fail_result():
+    marks = [30, 35, 25]
+    assert get_result(marks) == "FAIL"
